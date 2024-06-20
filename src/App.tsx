80fc5ce9e1fe9c9d57722e798ee7cb81839be5c7
안창완 <ssdii66@gmail.com>
2024-06-20 17:26:16 +0900

    variants

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,57 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", duration: 2 } },
+const Circle = styled(motion.div)`
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  border-radius: 35px;
+  background-color: white;
+`;
+
+const boxVariants = {
+  start: { opacity: 0, scale: 0 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "stpring",
+      duration: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const circlrVariants = {
+  start: { opacity: 0, y: 10 },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end"></Box>
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circlrVariants} />
+        <Circle variants={circlrVariants} />
+        <Circle variants={circlrVariants} />
+        <Circle variants={circlrVariants} />
+      </Box>
     </Wrapper>
   );
 }
